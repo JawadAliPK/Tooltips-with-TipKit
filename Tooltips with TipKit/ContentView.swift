fd166cdf970185deff686ea74a8936a89de98676
Jawad Ali <jawad@eddress.co>
2023-11-26 21:21:28 +0100

    Basic Mock data & UI setup

--- Tooltips with TipKit/ContentView.swift
+++ Tooltips with TipKit/ContentView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colors = MockData.colors
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                ForEach(colors, id: \.self) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill($0.gradient)
+                        .frame(height: 100)
+                        .contextMenu {
+                            Button("Favorite", systemImage: "star") {
+                                
+                            }
+                        }
+                }
+            }
+            .padding()
+            .navigationTitle("Colors")
+            .toolbar {
+                Button {
+                    colors.insert(.random, at: 0)
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension Color {
+    static var random: Color {
+        return Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
+    }
+}
+
+struct MockData {
+    static let colors = [Color.random,
+                         Color.random,
+                         Color.random,
+                         Color.random
+    ]
 }
