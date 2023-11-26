1d521702fb11568459650440f77deada9f646365
Jawad Ali <jawad@eddress.co>
2023-11-26 19:28:33 +0100

    Initial Commit

---
+++ Tooltips with TipKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tooltips with TipKit
+//
+//  Created by jawad ali on 26/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tooltips with TipKit/Tooltips_with_TipKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tooltips_with_TipKitApp.swift
+//  Tooltips with TipKit
+//
+//  Created by jawad ali on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tooltips_with_TipKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
